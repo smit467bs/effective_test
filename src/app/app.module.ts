0af1f8baf7cee6bc6532997308ab6bb0f51409db
Vyacheslav Byrdaev <smit467bs@gmail.com>
2023-08-24 11:09:51 +0300

    init: header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
 	imports: [
 		BrowserModule,
